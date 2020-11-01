f46c1ebf796daa388133f27d7279cd7e9b01d14c
esteban <esteban.munch.jones@gmail.com>
2020-11-01 14:07:00 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+
+import './App.css';
+import './web-components/search-result';
+
+
+function App() {
+
+  const [name, setName] = useState('');
+
+  return (
+    <div className="App">
+      
+      <input 
+      placeholder="Enter your name" 
+      onChange={(event) => setName(event.target.value)}
+      value={name}></input>
+
+      <div class="greeting">Hello {name}!</div>
+
+      <search-result name-attribute={name}></search-result>
+
+  </div>
+  );
+}
+
+export default App;
+
